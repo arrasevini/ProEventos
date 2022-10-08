5186444ed2c53b0039e1afc9d7b4e828b3804ba6
Vinícius Andrade <arrase.vini@gmail.com>
2022-10-08 15:30:10 -0300

    Layout Responsivo + Forms

---
+++ Front/ProEventos-App/src/app/components/eventos/evento-lista/evento-lista.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Evento } from 'src/app/models/Evento';
+import { EventoService } from 'src/app/services/evento.service';
+
+@Component({
+  selector: 'app-evento-lista',
+  templateUrl: './evento-lista.component.html',
+  styleUrls: ['./evento-lista.component.scss'],
+})
+export class EventoListaComponent implements OnInit {
+  modalRef?: BsModalRef;
+
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+  public widthImg: number = 150;
+  public marginImg: number = 2;
+  public showImg: boolean = true;
+  private _filtroLista: string = '';
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.spinner.show();
+    this.getEventos();
+  }
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  public getEventos(): void {
+    this.eventoService.getEventos().subscribe({
+      next: (_eventos: Evento[]) => {
+        this.eventos = _eventos;
+        this.eventosFiltrados = _eventos;
+      },
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastr.error('Houve um erro ao carregar os eventos.', 'Erro!');
+      },
+      complete: () => this.spinner.hide(),
+    });
+  }
+
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (a: { tema: string; local: string }) =>
+        a.tema.toLocaleLowerCase().indexOf(filtrarPor) != -1 ||
+        a.local.toLocaleLowerCase().indexOf(filtrarPor) != -1
+    );
+  }
+
+  public showHideImg(): void {
+    this.showImg = !this.showImg;
+  }
+
+  // ngx bootstrap
+  public openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+
+  public confirm(): void {
+    this.modalRef?.hide();
+    this.toastr.success('O evento foi deletado com sucesso.', 'Deletado!');
+  }
+
+  public decline(): void {
+    this.modalRef?.hide();
+  }
+
+  public detalheEvento(id: number): void {
+    this.router.navigate([`eventos/detalhe/${id}`]);
+  }
+}
